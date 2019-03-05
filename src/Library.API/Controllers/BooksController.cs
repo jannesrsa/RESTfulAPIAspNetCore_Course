775af391440f3420eb47609e9ddf3b4c8c2b0883
Jannes Coetzee <jannesrsa@gmail.com>
2019-03-05 15:23:12 +0200

    Add BooksController

---
+++ src/Library.API/Controllers/BooksController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authors/{authorId}/books")]
+    public class BooksController : Controller
+    {
+        private readonly ILibraryRepository _libraryRepository;
+
+        public BooksController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetBookForAuthor(Guid authorId, Guid id)
+        {
+            if (!_libraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var bookFromRep = _libraryRepository.GetBookForAuthor(authorId, id);
+            if (bookFromRep == null)
+            {
+                return NotFound();
+            }
+
+            var book = Mapper.Map<BookDto>(bookFromRep);
+
+            return Ok(book);
+        }
+
+        [HttpGet]
+        public IActionResult GetBooksForAuthor(Guid authorId)
+        {
+            if (!_libraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
+            }
+
+            var booksFromRep = _libraryRepository.GetBooksForAuthor(authorId);
+
+            var books = Mapper.Map<IEnumerable<BookDto>>(booksFromRep);
+
+            return Ok(books);
+        }
+    }
+}
