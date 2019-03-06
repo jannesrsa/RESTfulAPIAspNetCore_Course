2b5ddffe5e4a5227ffdd904492c1603884ca1d6c
Jannes Coetzee <jannesrsa@gmail.com>
2019-03-06 10:59:19 +0200

    Add AuthorCollectionsController

---
+++ src/Library.API/Controllers/AuthorCollectionsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
+using Library.API.Models;
+using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.API.Controllers
+{
+    [Route("api/authorcollections")]
+    public class AuthorCollectionsController : Controller
+    {
+        private readonly ILibraryRepository _libraryRepository;
+
+        // Inject repository with constructor injection
+        public AuthorCollectionsController(ILibraryRepository libraryRepository)
+        {
+            _libraryRepository = libraryRepository;
+        }
+
+        [HttpPost]
+        public IActionResult CreateAuthorCollection([FromBody] IEnumerable<AuthorForCreationDto> authorCollection)
+        {
+            if (authorCollection == null)
+            {
+                return BadRequest();
+            }
+
+            var authorsEntity = Mapper.Map<IEnumerable<Entities.Author>>(authorCollection);
+
+            foreach (var authorEntity in authorsEntity)
+            {
+                _libraryRepository.AddAuthor(authorEntity);
+            }
+
+            if (!_libraryRepository.Save())
+            {
+                throw new Exception("Creating an author collection failed on save.");
+            }
+
+            return Ok();
+
+            //var authorToReturn = Mapper.Map<AuthorDto>(authorEntity);
+
+            //return CreatedAtRoute(
+            //    "GetAuthor",
+            //    new { id = authorToReturn.Id },
+            //    authorToReturn
+            //);
+        }
+    }
+}
