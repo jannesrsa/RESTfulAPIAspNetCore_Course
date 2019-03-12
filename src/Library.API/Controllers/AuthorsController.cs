fa18ce44bc9d5a409155a2e625ffd81cfa1e4724
Jannes Coetzee <jannesrsa@gmail.com>
2019-03-12 14:40:21 +0200

    HttpHead

--- src/Library.API/Controllers/AuthorsController.cs
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -147,6 +147,7 @@ namespace Library.API.Controllers
         }
 
         [HttpGet(Name = "GetAuthors")]
+        [HttpHead]
         public IActionResult GetAuthors(AuthorsResourceParameters authorsResourceParameters,
             [FromHeader(Name = "Accept")] string mediaType)
         {
